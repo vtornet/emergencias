5a951801e9f3d7f68db48aac1233e61834d91602
Vtornet <vtornet@gmail.com>
2026-04-06 13:05:21 +0200

    Fix favicon 404 - add link to favicon.png in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,8 @@
 </script>
 
 <svelte:head>
+	<link rel="icon" type="image/png" href="/favicon.png" />
+	<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
 	<meta name="apple-mobile-web-app-capable" content="yes" />
 	<meta name="mobile-web-app-capable" content="yes" />
 </svelte:head>
